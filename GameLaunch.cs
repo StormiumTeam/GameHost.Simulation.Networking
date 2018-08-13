ba383d8aca2ff1002ae84e3523eb34a0d903648d
Guerro323 <kaltobattle@gmail.com>
2018-08-13 15:56:30 +0200

    Added an easy way to know if it's a server instance

---
+++ GameLaunch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public static class GameLaunch
+    {
+        public static bool HasGraphics { get; private set; }
+        public static bool IsServer { get; private set; }
+
+        static GameLaunch()
+        {
+            var commandLineOptions = Environment.CommandLine;
+            
+            IsServer = Application.isBatchMode || commandLineOptions.Contains("-batchmode");
+            HasGraphics = !(SystemInfo.graphicsDeviceID == 0 || commandLineOptions.Contains("-nographics"));
+        }
+    }
+}
